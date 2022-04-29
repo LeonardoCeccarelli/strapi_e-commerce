4c592b4e3df474586f1a8f42d127e643544775fc
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-04-29 09:28:42 +0200

    client with vuejs create

---
+++ client/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from "axios"
+import VueAxios from "vue-axios"
+import router from './router'
+
+const app = createApp(App)
+
+app.use(VueAxios, axios)
+app.use(router)
+
+app.mount('#app')
---
+++ client/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Pagina con vue</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

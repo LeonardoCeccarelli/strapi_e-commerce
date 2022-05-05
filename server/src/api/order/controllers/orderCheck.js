92b1ca6cac260feff2061e0df15c35548db7451a
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-05-05 10:24:02 +0200

    some modify

--- server/src/api/order/controllers/orderCheck.js
+++ server/src/api/order/controllers/orderCheck.js
@@ -1,42 +1,5 @@
 module.exports = {
     async check(ctx) {
-        const products = await strapi.db.query('api::product.product').findMany({
-            where: { id: JSON.parse(ctx.params.id) },
-            populate: { category: true, user: true },
-        });
-
-        const dataResponse = []
-        let totPrice = 0
-        const quantities = JSON.parse(ctx.params.quantity)
-        index = 0
-        products.forEach(item => {
-            // Calcolo quantità prodotto
-            const quantity = quantities[index]
-
-            // Calcolo dati prezzo sconto e prezzo finale dei vari prodotti
-            const normalPrice = item.price
-            const scount = item.scount ? item.scount : 0
-            let finalPrice = normalPrice - (normalPrice / 100 * scount)
-            // Lo moltiplico per la quantità
-            finalPrice = finalPrice * quantity
-            finalPrice = parseFloat(finalPrice.toFixed(2))
-
-            // Calcolo prezzo totale
-            totPrice = totPrice + finalPrice
-            totPrice = parseFloat(totPrice.toFixed(2))
-
-            dataResponse.push({
-                title: item.name,
-                normalPrice,
-                scount,
-                finalPrice,
-                quantity
-            })
-            index++
-        });
-        return {
-            data: dataResponse,
-            totPrice
-        }
+        return await strapi.calculatePrice(ctx.params.id, ctx.params.quantity)
     },
 };
--- server/src/index.js
+++ server/src/index.js
@@ -17,8 +17,42 @@ module.exports = {
    * run jobs, or perform some special logic.
    */
   bootstrap({ strapi }) {
-    strapi.calculateOrder = (Array) => {
+    strapi.calculatePrice = async (productsId, quantityId) => {
+      const products = await strapi.db.query('api::product.product').findMany({
+        where: { id: JSON.parse(productsId) },
+        populate: { category: true, user: true },
+      });
+      const quantities = JSON.parse(quantityId)
+      const dataResponse = []
+      let totPrice = 0
+      products.forEach((item, index) => {
+        // Calcolo quantità prodotto
+        const quantity = quantities[index]
 
+        // Calcolo dati prezzo sconto e prezzo finale dei vari prodotti
+        const normalPrice = item.price
+        const scount = item.scount ? item.scount : 0
+        let finalPrice = normalPrice - (normalPrice / 100 * scount)
+        // Lo moltiplico per la quantità
+        finalPrice = finalPrice * quantity
+        finalPrice = parseFloat(finalPrice.toFixed(2))
+
+        // Calcolo prezzo totale
+        totPrice = totPrice + finalPrice
+        totPrice = parseFloat(totPrice.toFixed(2))
+
+        dataResponse.push({
+          title: item.name,
+          normalPrice,
+          scount,
+          finalPrice,
+          quantity
+        })
+      });
+      return {
+        data: dataResponse,
+        totPrice
+      }
     }
   },
 };
